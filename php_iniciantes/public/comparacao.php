cbbc1d018430331ff80deb0087d38691e09d8ad5
Marcelo Teixeira <marcelo.estudo.cefet@gmail.com>
2026-01-26 17:06:12 -0300

    Aula sobre operadores de comparação em PHP

---
+++ php_iniciantes/public/comparacao.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+    Os operadores de comparação em PHP são utilizados para comparar dois valores
+    Eles retornam um valor booleano (true ou false) dependendo do resultado da comparação
+
+    São eles:
+    Igual (==)
+    Idêntico (===)
+    Diferente (!= ou <>)
+    Não idêntico (!==)
+    Maior que (>)
+    Menor que (<)
+
+    Normalmente são utilizados em estruturas condicionais como if, else, switch, etc.
+*/
