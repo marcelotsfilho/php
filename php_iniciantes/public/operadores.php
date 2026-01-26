8ca2f03589ed23ba8d1841c00972f913acf3ef76
Marcelo Teixeira <marcelo.estudo.cefet@gmail.com>
2026-01-26 12:49:11 -0300

    Aula sobre operadores em PHP

---
+++ php_iniciantes/public/operadores.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+    Em PHP existem diversos operadores aritméticos que podem ser utilizados
+
+    soma (+)
+    subtração (-)
+    multiplicação (*)
+    divisão (/)
+    módulo (%)
+    exponenciação (**)
+    incremento (++)
+    decremento (--)
+
+*/
+
+$number1 = 10;
+$number2 = 2;
+
+echo 'Soma: ' . ($number1 + $number2). '</br>';
+echo 'Subtração: ' . ($number1 - $number2). '</br>';
+echo 'Multiplicação: ' . ($number1 * $number2). '</br>';
+echo 'Divisão: ' . ($number1 / $number2). '</br>';
+echo 'Módulo: ' . ($number1 % $number2). '</br>';
+echo 'Exponenciação: ' . ($number1 ** $number2). '</br>';
+
+// O incremento e decremento podem ser feitos em condicionais
