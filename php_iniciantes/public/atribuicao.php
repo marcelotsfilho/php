a401b229332afdbb17449675b8fa1faf31b0ef85
Marcelo Teixeira <marcelo.estudo.cefet@gmail.com>
2026-01-26 17:00:01 -0300

    Aula sobre operadores de atribuição em PHP

---
+++ php_iniciantes/public/atribuicao.php
@@ -0,0 +1,11 @@
+<?php
+
+$name = 'Marcelo';
+$name = $name . ' Teixeira';
+
+// Maneira mais simples de ser feito
+
+$name .= ' Teixeira';
+
+// O .= é um operador de atribuição que concatena valores
+echo $name . '</br>';
